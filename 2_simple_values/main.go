23dbaebea889aeb78c806d3d17015ce24f991a30
vivekbargude <vivekbargude1206@gmail.com>
2025-12-24 11:53:24 +0530

    1. Simple Values

---
+++ 2_simple_values/main.go
@@ -0,0 +1,18 @@
+package main 
+
+import "fmt";
+
+func main() {
+	//integer value
+	fmt.Println(42)
+
+	//string value
+	fmt.Println("Hello Golang")
+
+	//bool value
+	fmt.Println(true)
+
+	//float value
+	fmt.Println(3.14)
+	fmt.Println(2.7/1.0)
+}
